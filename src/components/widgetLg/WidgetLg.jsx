b864e75acc66b9ddc5ef758ea55e45703996a4cb
ecabigting <ericcabigting@outlook.com>
2021-12-04 18:45:05 +0400

    added new large widget component

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,9 +1,69 @@
 import "./widgetLg.css"
 
 export default function WidgetLg() {
+    const Button = ({type}) => {
+        return <button className={"widgetLgButton "+type}>{type}</button>;
+    }
     return (
         <div className="widgetLg">
-            Large
+            <h3 className="widgetLgTitle">
+                Latest Transactions
+            </h3>
+            <table className="widgetLgTable">
+                <tr className="widgetLgTr">
+                    <th className="widgetLgTh">
+                        Customer
+                    </th>
+                    <th className="widgetLgTh">
+                        Date
+                    </th>
+                    <th className="widgetLgTh">
+                        Amount
+                    </th>
+                    <th className="widgetLgTh">
+                        Status
+                    </th>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://picjumbo.com/wp-content/uploads/woman-in-a-glittering-sequin-party-dress-at-the-celebration-party-free-photo-2210x3315.jpg"
+                         alt="" 
+                         className="widgetLgImg" />
+                         <span className="widgetLgName">Big Berta</span>
+                    </td>
+                    <td className="widgetLgDate">2 Dec 2021</td>
+                    <td className="widgetLgAmount">$ 150.00</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Approve"/>
+                    </td>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://picjumbo.com/wp-content/uploads/woman-in-a-glittering-sequin-party-dress-at-the-celebration-party-free-photo-2210x3315.jpg"
+                         alt="" 
+                         className="widgetLgImg" />
+                         <span className="widgetLgName">Big Berta</span>
+                    </td>
+                    <td className="widgetLgDate">2 Dec 2021</td>
+                    <td className="widgetLgAmount">$ 150.00</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Decline"/>
+                    </td>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://picjumbo.com/wp-content/uploads/woman-in-a-glittering-sequin-party-dress-at-the-celebration-party-free-photo-2210x3315.jpg"
+                         alt="" 
+                         className="widgetLgImg" />
+                         <span className="widgetLgName">Big Berta</span>
+                    </td>
+                    <td className="widgetLgDate">2 Dec 2021</td>
+                    <td className="widgetLgAmount">$ 150.00</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Pending"/>
+                    </td>
+                </tr>
+            </table>
         </div>
     )
 }
