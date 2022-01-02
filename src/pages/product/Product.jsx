f4be371e1e9328f0d97b090f88995fee6adfeb67
ecabigting <ericcabigting@outlook.com>
2022-01-02 16:14:20 +0400

    update more details info for product details

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -18,11 +18,59 @@ export default function Product({userId}) {
                     <Chart data={ productData } dataKey="Sales" title="Product Performance"/>
                 </div>
                 <div className="productTopRight">
-
+                    <div className="productInfoTop">
+                        <img src="https://images.pexels.com/photos/3687999/pexels-photo-3687999.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="productInfoImg" />
+                        <span className="productName">Products</span>
+                    </div>
+                    <div className="productInfoBottom">
+                        <div className="productInfoItem">
+                            <span className="productInfoKey">id:</span>
+                            <span className="productInfoValue">123</span>
+                        </div>
+                        <div className="productInfoItem">
+                            <span className="productInfoKey">sales:</span>
+                            <span className="productInfoValue">$423</span>
+                        </div>
+                        <div className="productInfoItem">
+                            <span className="productInfoKey">active:</span>
+                            <span className="productInfoValue">Yes</span>
+                        </div>
+                        <div className="productInfoItem">
+                            <span className="productInfoKey">in stock:</span>
+                            <span className="productInfoValue">Yes</span>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="productBottonm">
-
+                <form className="productForm">
+                    <div className="productFormLeft">
+                        <label>Product Name</label>
+                        <input type="text" placeholder="Products"/>
+                        <label>In Stock</label>
+                        <select name="inStock" id="inStock">
+                            <option value="true">Yes</option>
+                            <option value="false">No</option>
+                        </select>
+                        <label>Status</label>
+                        <select name="status" id="status">
+                            <option value="true">Yes</option>
+                            <option value="false">No</option>
+                        </select>
+                    </div>
+                    <div className="productFormRight">
+                        <div className="productUpload">
+                            <img src="https://images.pexels.com/photos/3687999/pexels-photo-3687999.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="productUploadImage" />
+                            <label for="file">
+                                <Publish/>
+                            </label>
+                            <input type="file" name="file" id="file" style={{display:"none"}}/>
+                        </div>
+                        <button className="productButton">
+                            Update
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     )
